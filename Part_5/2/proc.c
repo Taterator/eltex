9dffd883bd87a05bdd69491a189fd052880f9412
Taterator <filato-roma@yandex.ru>
2025-08-21 20:09:06 +0700

    p5 t2&3

---
+++ Part_5/2/proc.c
@@ -0,0 +1,56 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <linux/sched.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+ 
+#define MSG_SIZE 127
+#define PROC_NAME "proc_msg_test"
+#define MODE 0666 #Чтобы обращаться мог не только рут
+ 
+static int len,temp;
+static char *msg;
+ 
+ssize_t read_proc(struct file *filp, char *buf, size_t count, loff_t *offp ) {
+    if(count > temp) {
+        count = temp;
+    }
+    temp = temp - count;
+    copy_to_user(buf, msg, count);
+    if(count == 0)
+        temp = len;
+    return count;
+}
+ 
+ssize_t write_proc(struct file *filp, const char *buf, size_t count, loff_t *offp) {
+    copy_from_user(msg, buf, count);
+    len = count;
+    temp = len;
+    return count;
+}
+ 
+static const struct proc_ops proc_fops = {
+    proc_read: read_proc,
+    proc_write: write_proc,
+};
+ 
+void create_new_proc_entry(void) { //use of void for no arguments is compulsory now
+    proc_create(PROC_NAME, MODE, NULL, &proc_fops);
+    msg = kmalloc(MSG_SIZE * sizeof(char), GFP_KERNEL);
+}
+ 
+int proc_init (void) {
+    create_new_proc_entry();
+    return 0;
+}
+ 
+void proc_cleanup(void) {
+    remove_proc_entry(PROC_NAME, NULL);
+    kfree(msg);
+}
+ 
+MODULE_LICENSE("NONAME");
+MODULE_AUTHOR("FilatovR");
+module_init(proc_init);
+module_exit(proc_cleanup);
